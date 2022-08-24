08202f5b25f3695b3119b1f50f7ffd966a51f054
Dave Freeman <sugardave@gmail.com>
2022-08-24 04:27:03 -0500

    add some reusable utilities

---
+++ src/lib/utils.ts
@@ -0,0 +1,26 @@
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
+import { getDb } from "../store/db";
+
+// API Server
+export const getConsoleServer = (
+  location: "local" | "remote" = "remote",
+  port?: number
+) => {
+  const devMode = process.env.NODE_ENV === "development";
+  const local = location === "local";
+  const host = local ? "http://localhost" : "https://console.bls.dev";
+  return `${host}:${port ? port : devMode && local ? 3000 : 443}`;
+};
+
+// JWT retrieval
+
+export const getTokenFromStore = () => {
+  const db = getDb();
+  const { token } = db.get("config").value();
+  return token;
+};
+
+// Node/npm config
+export const getNpmConfigInitVersion = (): string =>
+  execSync("npm config get init-version").toString("utf-8");
