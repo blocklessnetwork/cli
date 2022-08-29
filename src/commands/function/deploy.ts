187aecc38cb0c6733598d8047ab007248b9974ca
Dave Freeman <sugardave@gmail.com>
2022-08-29 16:48:33 -0500

    separate publish from deploy

--- src/commands/function/deploy.ts
+++ src/commands/function/deploy.ts
@@ -1,11 +1,8 @@
-import { existsSync, readFileSync, writeFileSync } from "fs";
-import { createHash, BinaryToTextEncoding } from "crypto";
 import Chalk from "chalk";
-import { execSync } from "child_process";
-import FormData from "form-data";
 import { getToken } from "../../store/db";
 import axios from "axios";
-import { IDeploymentOptions, IManifest } from "./interfaces";
+import { IDeploymentOptions } from "./interfaces";
+import { run as runPublish } from "./publish";
 
 const deploymentOptions: IDeploymentOptions = {
   functionId: "",
@@ -13,76 +10,17 @@ const deploymentOptions: IDeploymentOptions = {
   userFunctionId: "",
 };
 
-const consoleServer = "https://console.bls.dev";
-const wasiServer = "https://wasi.bls.dev";
+const server = "https://console.bls.dev";
 const token = getToken();
-
-const createChecksum = ({
-  digest = "hex",
-  filePath = "",
-  hash = "sha256",
-}): string => {
-  console.log(Chalk.yellow(`Creating hash...`));
-  const sha = createHash(hash);
-  const fileBuffer = readFileSync(`${filePath}`);
-  sha.update(fileBuffer);
-  return sha.digest(digest as BinaryToTextEncoding);
-};
-
-const createManifest = (
-  buildDir: string,
-  entry: string,
-  url: string
-): IManifest => {
-  const name = entry.split(".")[0];
-  const manifest: IManifest = {
-    id: "",
-    name,
-    description: "",
-    fs_root_path: "./",
-    entry,
-    runtime: {
-      checksum: createChecksum({ filePath: `${buildDir}/${url}` }),
-      url,
-    },
-  };
-  return manifest;
-};
-
-const renameWasm = (path: string, oldName: string, newName: string) => {
-  execSync(`mv ${oldName} ${newName}`, { cwd: path, stdio: "inherit" });
-};
-
-const publishFunction = async (manifest: any, archive: any, cb?: Function) => {
-  const formData = new FormData();
-
-  formData.append("manifest", manifest);
-  formData.append("wasi_archive", archive);
-
-  axios
-    .post(`${wasiServer}/api/submit`, formData, {
-      headers: {
-        "Authorization": `Bearer ${token}`,
-        "Content-Type": "multipart/form-data",
-      },
-    })
-    .then((res) => {
-      if (cb) {
-        cb(res.data);
-      }
-    })
-    .catch((error) => {
-      console.log("error publishing function", error);
-    });
-};
 
 //TODO: make this a lot better.
 const deployFunction = (data: any) => {
   const { cid: functionId, name: functionName } = data;
   const { userFunctionId } = deploymentOptions;
+  console.log(Chalk.yellow(`Deploying ${functionName}`));
   axios
     .post(
-      `${consoleServer}/api/modules/deploy`,
+      `${server}/api/modules/deploy`,
       {
         functionId,
         functionName,
@@ -103,57 +41,13 @@ const deployFunction = (data: any) => {
 };
 
 export const run = (options: any) => {
-  const { debug, path, rebuild } = options;
-  // check for and store unmodified wasm file name to change later
-  const defaultWasm = debug ? "debug.wasm" : "release.wasm";
-  const buildDir = `${path}/build`;
-  const pathParts = path.split("/");
-  const name = pathParts.pop();
-  const wasmName = `${name}.wasm`;
-  const wasmArchive = `${name}.tar.gz`;
+  const { debug, name, path, rebuild } = options;
   const {
     bls: { functionId: userFunctionId },
   } = require(`${path}/package`);
 
-  //TODO: this is absolutely monstrous and needssanity appplied
+  //TODO: this is absolutely monstrous and needs sanity applied
   deploymentOptions.userFunctionId = userFunctionId;
 
-  if (rebuild) {
-    console.log(Chalk.green(`Building function ${name} in ${buildDir}...`));
-    execSync(
-      `npm run build:${debug ? "debug" : "release"}; ${renameWasm(
-        buildDir,
-        defaultWasm,
-        wasmName
-      )}`,
-      { cwd: path, stdio: "inherit" }
-    );
-  } else if (existsSync(`${buildDir}`)) {
-    if (existsSync(`${buildDir}/${defaultWasm}`)) {
-      renameWasm(buildDir, defaultWasm, wasmName);
-    }
-  } else {
-    console.log(
-      Chalk.red(`Could not access ${buildDir}.  Has the function been built?`)
-    );
-  }
-
-  console.log(Chalk.yellow(`Creating tarball...`));
-  execSync(`tar zcf ${wasmArchive} ${wasmName}`, {
-    cwd: buildDir,
-    stdio: "inherit",
-  });
-
-  console.log(Chalk.yellow(`Creating manifest...`));
-  writeFileSync(
-    `${buildDir}/manifest.json`,
-    JSON.stringify(createManifest(buildDir, wasmName, wasmArchive))
-  );
-
-  console.log(Chalk.yellow(`Deploying function located in ${buildDir}`));
-  publishFunction(
-    readFileSync(`${buildDir}/manifest.json`),
-    readFileSync(`${buildDir}/${wasmArchive}`),
-    deployFunction
-  );
+  runPublish({ debug, name, path, publishCallback: deployFunction, rebuild });
 };
