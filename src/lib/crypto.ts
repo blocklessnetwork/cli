3947def4b6e419fe9d29b0bea123d0ecf5d27a2e
Udit <udit@eskaytech.com>
2022-11-29 21:41:43 +0530

    Print wallet address in the whoami command

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -7,3 +7,14 @@ import crypto from "crypto";
 export const generateChecksum = (archive: Buffer): string => {
   return crypto.createHash("sha256").update(archive).digest("hex");
 };
+
+
+/**
+ * A simple function to parse JWT from a given token 
+ * 
+ * @param token 
+ * @returns 
+ */
+export const parseJwt = (token: string) => {
+  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
+}
