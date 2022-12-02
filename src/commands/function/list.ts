8e381d253df4f1113a1b44876f426ec9f8ff14aa
Udit <udit@eskaytech.com>
2022-12-02 16:23:43 +0530

    Update console authentication error, ENG-419

--- src/commands/function/list.ts
+++ src/commands/function/list.ts
@@ -1,11 +1,12 @@
 import Table from "cli-table3"
 import { consoleClient } from "../../lib/http"
+import { logger } from "../../lib/logger"
 
 export const run = async () => {
   try {
     const { data } = await consoleClient.post(`/api/modules/mine`, {})
 
-    const maxWidth = Math.min(process.stdout.columns, 120) || 120;
+    const maxWidth = Math.min(process.stdout.columns, 120) || 120
     var table = new Table({
       head: ["Name", "CID", "Status"],
       colWidths: [28, maxWidth - 50, 15],
@@ -17,9 +18,9 @@ export const run = async () => {
       table.push([f.functionName, f.functionId, f.status])
     })
 
-    console.log(table.toString())
-  } catch (error) {
-    console.log('Failed to retrieve function list')
+    logger.log(table.toString())
+  } catch (error: any) {
+    logger.error('Failed to retrieve function list.', error.message)
     return
   }
 }
