5121e590c6c916c770dd36848adddf38c390568c
Udit <udit@eskaytech.com>
2022-12-06 18:52:10 +0530

    Verify JWT expiry time before confirming wallet connection

--- src/commands/wallet/list.ts
+++ src/commands/wallet/list.ts
@@ -3,22 +3,23 @@ import Table from "cli-table3"
 import { parseJwt } from "../../lib/crypto"
 import { getToken } from "../../store/db"
 
-// todo verify the jwt is valid
 export const listWallet = () => {
   const token = getToken()
 
   let publicAddress
   let walletType
+  let expiredTime = 0
 
   if (token) {
     try {
       const jwt = parseJwt(token)
       publicAddress = jwt.publicAddress
       walletType = jwt.walletType
+      expiredTime = new Date(jwt.exp * 1000).getTime()
     } catch {}
   }
 
-  if (!!publicAddress && !!walletType) {
+  if (!!publicAddress && !!walletType && expiredTime > Date.now()) {
     console.log(`You are connected with a JWT token to the Blockless console!`)
 
     var table = new Table({
@@ -31,7 +32,7 @@ export const listWallet = () => {
     console.log(table.toString())
   } else {
     console.log(
-      `Wallet is currently ${Chalk.yellow(
+      `Wallet is currently ${Chalk.red(
         "disconnected"
       )}\nPlease login using the ${Chalk.yellow("bls login")} command`
     );
