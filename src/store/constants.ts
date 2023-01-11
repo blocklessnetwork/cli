545bdb74c99b1874034d1af53e4a46ea04f4dd37
Udit <udit@eskaytech.com>
2023-01-11 20:57:04 +0530

    Modify redirect query param to match updated dashboard format

--- src/store/constants.ts
+++ src/store/constants.ts
@@ -1,4 +1,4 @@
 export const NAME = "bls";
 export const CONSOLE_HOST_LOCAL = "http://localhost:3005";
-export const CONSOLE_HOST_REMOTE = "https://console.bls.dev";
+export const CONSOLE_HOST_REMOTE = "https://dashboard.blockless.network";
 export const TMP_DIR = '/tmp/blstmpdir'
