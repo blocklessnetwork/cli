214edd58c521cdf786e45d3295cd0c0009c8f371
Derek Anderson <dmikey@users.noreply.github.com>
2024-01-31 09:46:32 -0600

    add support for archive download
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

--- src/commands/function/build.ts
+++ src/commands/function/build.ts
@@ -1,73 +1,79 @@
-import fs from "fs"
-import Chalk from 'chalk'
-import { resolve } from "path"
-import { buildWasm, createWasmArchive, createWasmManifest } from "./shared"
-import { parseBlsConfig } from "../../lib/blsConfig"
-import { generateMd5Checksum } from "../../lib/crypto"
-import { logger } from "../../lib/logger"
-import { slugify } from "../../lib/strings"
+import fs from "fs";
+import Chalk from "chalk";
+import { resolve } from "path";
+import { buildWasm, createWasmArchive, createWasmManifest } from "./shared";
+import { parseBlsConfig } from "../../lib/blsConfig";
+import { generateMd5Checksum, generateSha256Checksum } from "../../lib/crypto";
+import { logger } from "../../lib/logger";
+import { slugify } from "../../lib/strings";
 
 /**
  * Execute the `build` command line operation
- * 
- * @param options 
+ *
+ * @param options
  * @returns void
  */
 export const run = (options: {
-  path: string
-  debug: boolean
-  rebuild: boolean
+	path: string;
+	debug: boolean;
+	rebuild: boolean;
 }) => {
-  const {
-    debug = true,
-    path = process.cwd(),
-    rebuild = true
-  } = options
+	const { debug = true, path = process.cwd(), rebuild = true } = options;
 
-  try {
-    // Fetch BLS config
-    const { name, content_type, deployment, build, build_release } = parseBlsConfig()
+	try {
+		// Fetch BLS config
+		const { name, content_type, deployment, build, build_release } =
+			parseBlsConfig();
 
-    // check for and store unmodified wasm file name to change later
-    const buildConfig = !debug ? build_release : build
-    const deployConfig = deployment
-    const buildName = buildConfig.entry ? slugify(buildConfig.entry.replace('.wasm', '')) : slugify(name)
-    const buildDir = resolve(path, buildConfig.dir || 'build')
-    const wasmName = slugify(buildConfig.entry) || `${slugify(name)}.wasm`
-    const wasmArchive = `${buildName}.tar.gz`
+		// check for and store unmodified wasm file name to change later
+		const buildConfig = !debug ? build_release : build;
+		const deployConfig = deployment;
+		const buildName = buildConfig.entry
+			? slugify(buildConfig.entry.replace(".wasm", ""))
+			: slugify(name);
+		const buildDir = resolve(path, buildConfig.dir || "build");
+		const wasmName = slugify(buildConfig.entry) || `${slugify(name)}.wasm`;
+		const wasmArchive = `${buildName}.tar.gz`;
 
-    // Rebuild function if requested or 
-    if (!fs.existsSync(resolve(buildDir, wasmName)) || rebuild) {
-      buildWasm(wasmName, buildDir, path, buildConfig, debug)
-    } else if (fs.existsSync(resolve(buildDir, wasmName)) && !rebuild) {
-      return
-    }
+		// Rebuild function if requested or
+		if (!fs.existsSync(resolve(buildDir, wasmName)) || rebuild) {
+			buildWasm(wasmName, buildDir, path, buildConfig, debug);
+		} else if (fs.existsSync(resolve(buildDir, wasmName)) && !rebuild) {
+			return;
+		}
 
-    // Generate a default WASM manifest
-    const wasmManifest = createWasmManifest(wasmName, content_type)
+		// Generate a default WASM manifest
+		const wasmManifest = createWasmManifest(wasmName, content_type);
 
-    // Create a WASM archive
-    createWasmArchive(buildDir, wasmArchive, wasmName)
+		// Create a WASM archive
+		createWasmArchive(buildDir, wasmArchive, wasmName);
 
-    wasmManifest.modules?.push({
-      file: wasmName,
-      name: wasmName.split(".")[0],
-      type: 'entry',
-      md5: generateMd5Checksum(fs.readFileSync(`${buildDir}/${wasmName}`))
-    })
+		wasmManifest.runtime = {
+			url: wasmArchive,
+			checksum: generateSha256Checksum(
+				fs.readFileSync(`${buildDir}/${wasmArchive}`),
+			),
+		};
 
-    if (deployConfig) {
-      wasmManifest.permissions = deployConfig.permissions || []
-    }
+		wasmManifest.modules?.push({
+			file: wasmName,
+			name: wasmName.split(".")[0],
+			type: "entry",
+			md5: generateMd5Checksum(fs.readFileSync(`${buildDir}/${wasmName}`)),
+		});
 
-    // Store manifest
-    fs.writeFileSync(`${buildDir}/manifest.json`, JSON.stringify(wasmManifest))
+		if (deployConfig) {
+			wasmManifest.permissions = deployConfig.permissions || [];
+		}
 
-    // Show success message
-    console.log(`${Chalk.green('Build successful!')}`)
-    console.log('')
-  } catch (error: any) {
-    logger.error('Failed to build function.', error.message)
-    return
-  }
-}
+		// Store manifest
+		fs.writeFileSync(`${buildDir}/manifest.json`, JSON.stringify(wasmManifest));
+
+		// Show success message
+		console.log(`${Chalk.green("Build successful!")}`);
+		console.log("");
+	} catch (error: any) {
+		logger.error("Failed to build function.", error.message);
+		return;
+	}
+};
--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -5,20 +5,23 @@ import crypto from "crypto";
  * @returns
  */
 export const generateChecksum = (archive: Buffer): string => {
-  return crypto.createHash("sha256").update(archive).digest("hex");
+	return crypto.createHash("sha256").update(archive).digest("hex");
 };
 
 export const generateMd5Checksum = (buf: Buffer) => {
-  return crypto.createHash('md5').update(buf).digest("hex")
-}
+	return crypto.createHash("md5").update(buf).digest("hex");
+};
 
+export const generateSha256Checksum = (buf: Buffer) => {
+	return crypto.createHash("sha256").update(buf).digest("hex");
+};
 
 /**
- * A simple function to parse JWT from a given token 
- * 
- * @param token 
- * @returns 
+ * A simple function to parse JWT from a given token
+ *
+ * @param token
+ * @returns
  */
 export const parseJwt = (token: string) => {
-  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
-}
+	return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
+};
