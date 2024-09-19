4567cb5269bf9ec1530583ce86934d50b694c97f
Derek Anderson <dmikey@users.noreply.github.com>
2024-09-19 18:28:54 -0500

    updating min node version, and bash declare (#132)
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

--- dev-bin.js
+++ dev-bin.js
@@ -1,26 +1,17 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 require("ts-node/register");
-const compareVersions = require("compare-versions");
-const MIN_NODE_VERSION = "14.17.6";
 
-if (compareVersions.compare(process.versions.node, MIN_NODE_VERSION, "<")) {
-	console.error(
-		`Bls CLI requires at least node.js v${MIN_NODE_VERSION}.\nYou are using v${process.versions.node}. Please update your version of node.js. Consider using Node.js version manager https://github.com/nvm-sh/nvm.`,
-	);
-	process.exit(1);
-} else {
-	const { main } = require("./src/index");
-	const { version } = require("./package.json");
+const { main } = require("./src/index");
+const { version } = require("./package.json");
 
-	const runCLI = (args) => {
-		main(args, { version });
-	};
+const runCLI = (args) => {
+	main(args, { version });
+};
 
-	// Export the runCLI function for testing
-	module.exports = { runCLI };
+// Export the runCLI function for testing
+module.exports = { runCLI };
 
-	// Run the CLI if executed directly
-	if (require.main === module) {
-		runCLI(process.argv.slice(2));
-	}
+// Run the CLI if executed directly
+if (require.main === module) {
+	runCLI(process.argv.slice(2));
 }
